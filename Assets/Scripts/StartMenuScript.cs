4d28a196c8fb6d6e69287bb5321f694f36f7970d
MatK <mathew.kostrzewa@ontariotechu.net>
2020-10-06 15:23:09 -0400

    Imported dungeon assets from asset store

--- Assets/Scripts/StartMenuScript.cs
+++ Assets/Scripts/StartMenuScript.cs
@@ -1,6 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * Mathew Kostrzewa
+ * 100591924
+ * Game Engines Midterm
+ * Ontario Tech University
+ * Fall 2020
+ *
+ * Assets used: https://assetstore.unity.com/packages/3d/environments/dungeons/dungeon-traps-50655
+ *              https://assetstore.unity.com/packages/3d/environments/dungeons/decrepit-dungeon-lite-33936
+ */
+
 public class StartMenuScript : MonoBehaviour
 {
     public void StartGame()
