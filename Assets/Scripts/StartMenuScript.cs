6a859f5f60434af26622d934783ef0745b1d61a9
MatK <mathew.kostrzewa@ontariotechu.net>
2020-10-06 14:56:01 -0400

    Added start scene with working buttons, and empty play and end scenes

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void ViewPreviousStats()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+}
